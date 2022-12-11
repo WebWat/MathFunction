96a8ec03f75d1f377c223d23c99622a719542c88
Sergey <64882377+WebWat@users.noreply.github.com>
2022-12-11 20:42:44 +0300

    fix limit and add derivative

--- CompMath/Program.fs
+++ CompMath/Program.fs
@@ -9,18 +9,21 @@ type Position =
     | Right
 
 
-let limit (func: float -> float) (x: float) (accuracy: int) (position: Position) =          
+let limit (func: float -> float) (x: float) (accuracy: int) (position: Position) =   
+    if x = infinity || x = -infinity then raise (NotImplementedException())
+    if accuracy < 2 then raise (ArgumentException("Accuracy cannot be less than 2 digits?"))
+
     let result = func x
 
     if result = infinity || Double.IsNaN result then
         let needed = pown 10. -accuracy
 
-        let rec calculate approach eps ldiff =
+        let rec calculate approach eps ldiff mayBeInf =
             // Take two points on the interval
             let (first, second) = 
                  match position with
-                 | Position.Left -> (func (x - eps * 2.0), func (x - eps))
-                 | Position.Right -> (func (x + eps * 2.0), func (x + eps))
+                 | Position.Left -> (func (x - eps * 2.), func (x - eps))
+                 | Position.Right -> (func (x + eps * 2.), func (x + eps))
             
             let diff = abs(second - first)
 
@@ -32,16 +35,25 @@ let limit (func: float -> float) (x: float) (accuracy: int) (position: Position)
             // If the value of the function does not change
             if diff = 0 then second
             // If reached the specified accuracy OR the calculations are inconsistent
-            elif diff < needed || diff > ldiff then approach
-            // If the values tend to infinity
-            elif diff > eps * 10. then
-                if second > first then infinity else -infinity
-            else calculate second (eps * 0.1) diff
+            elif diff < needed then second
+            elif diff > ldiff then
+                if mayBeInf = true then 
+                    if second > first then infinity else -infinity
+                else
+                    second
+            else calculate second (eps * 0.1) diff false
 
-        calculate 0.0 0.1 10
+        calculate 0.0 0.01 (abs(func (x - 0.2) - func (x - 0.1))) true
     else
         result
 
+let derivative (func: float -> float) (points: float[]) =
+    Array.map (fun item -> 
+         limit (fun x -> ((func (item + x) - func item)/ x)) 0 3 Position.Left
+    ) points
 
-printfn "%.20f" (limit (fun x -> 1./log (1. + x) - 1. / x) 0 10 Position.Left)
+let func (x: float) = tan x
+
+printfn "%A" (derivative (fun x -> Math.Pow(x, x) * tan x) [|1..5|])
+
 Console.ReadLine() |> ignore

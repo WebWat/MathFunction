22045cff76a0de56ce12e10fc2a86e30f1be46fd
Sergey <64882377+WebWat@users.noreply.github.com>
2022-12-12 10:24:22 +0300

    first try

---
+++ CompMath/Main.fs
@@ -0,0 +1,59 @@
+﻿module Main
+
+open System
+
+exception NotExist
+
+type Position =
+    | Left
+    | Right
+
+let limit (func: float -> float) (x: float) (accuracy: int) (position: Position) =   
+    if x = infinity || x = -infinity then raise (NotImplementedException())
+    if accuracy < 2 then raise (ArgumentException("Accuracy cannot be less than 2 digits?"))
+
+    let result = func x
+
+    if result = infinity || Double.IsNaN result then
+        let needed = pown 10. -accuracy
+
+        let rec calculate eps ldiff mayBeInf =
+            // Take two points on the interval
+            let (first, second) = 
+                 match position with
+                 | Position.Left -> (func (x - eps * 2.), func (x - eps))
+                 | Position.Right -> (func (x + eps * 2.), func (x + eps))
+            
+            let diff = abs(second - first)
+
+            // If there is no function
+            if Double.IsNaN diff then raise (NotExist)
+
+            printfn "\nStart => %.20f\nEnd => %.20f\nDiff => %.20f\nEps  => %.20f\nldiff => %.20f" first second diff eps ldiff
+            
+            // If the value of the function does not change
+            if diff = 0 then second
+            // If reached the specified accuracy OR the calculations are inconsistent
+            elif diff < needed then second
+            elif diff > ldiff then
+                if mayBeInf = true then 
+                    if second > first then infinity else -infinity
+                else
+                    second
+            else calculate (eps * 0.1) diff false
+
+        let startDiff = abs(func (x - 0.2) - func (x - 0.1))
+        if Double.IsNaN startDiff then raise (NotExist)
+
+        calculate 0.01 startDiff true
+    else
+        result
+
+let derivative (func: float -> float) (points: float[]) =
+    Array.map (fun item ->
+        try
+            limit (fun x -> ((func (item + x) - func item)/ x)) 0 3 Position.Left
+        with
+        | _ -> -1
+    ) points
+
--- CompMath/Program.fs
+++ CompMath/Program.fs
@@ -1,59 +1,68 @@
-﻿module Main
-
-open System
-
-exception NotExist
-
-type Position =
-    | Left
-    | Right
+﻿open System
+open Main
 
 
-let limit (func: float -> float) (x: float) (accuracy: int) (position: Position) =   
-    if x = infinity || x = -infinity then raise (NotImplementedException())
-    if accuracy < 2 then raise (ArgumentException("Accuracy cannot be less than 2 digits?"))
+//printfn "%A" (derivative (fun x -> Math.Pow(x, x) * tan x * 1./log(x)) [|1..5|])
 
-    let result = func x
+type Node = 
+    {
+    Value: Option<int>
+    Operation: string
+    Left: Option<Node>
+    Right: Option<Node>
+    }
+    override this.ToString() =
+        let getData (value: Option<'a>) =
+            if value.IsSome then value.Value.ToString()
+            else "None"
 
-    if result = infinity || Double.IsNaN result then
-        let needed = pown 10. -accuracy
+        $"Value: [{getData(this.Value)}]; Operation: [{this.Operation}]; Left: [{getData(this.Left)}]; Right: [{getData(this.Right)}]"
 
-        let rec calculate eps ldiff mayBeInf =
-            // Take two points on the interval
-            let (first, second) = 
-                 match position with
-                 | Position.Left -> (func (x - eps * 2.), func (x - eps))
-                 | Position.Right -> (func (x + eps * 2.), func (x + eps))
-            
-            let diff = abs(second - first)
+let isNumber (number) =
+    let mutable temp = 0
+    Int32.TryParse(number.ToString(), &temp)
 
-            // If there is no function
-            if Double.IsNaN diff then raise (NotExist)
+let IsOne (symbol: char) (data: string) = 
+    let result = data.Split(symbol)
+    result.Length = 1
 
-            printfn "\nStart => %.20f\nEnd => %.20f\nDiff => %.20f\nEps  => %.20f\nldiff => %.20f" first second diff eps ldiff
-            
-            // If the value of the function does not change
-            if diff = 0 then second
-            // If reached the specified accuracy OR the calculations are inconsistent
-            elif diff < needed then second
-            elif diff > ldiff then
-                if mayBeInf = true then 
-                    if second > first then infinity else -infinity
-                else
-                    second
-            else calculate (eps * 0.1) diff false
-
-        calculate 0.01 (abs(func (x - 0.2) - func (x - 0.1))) true
+let rec convert (line: string) : Node =
+    if isNumber(line) then 
+        { Value = Some(int line); Operation = ""; Right = None; Left = None }
     else
-        result
+        if not (IsOne '*' line) then
+            let arr = line.Split('*');
+            let (lft, rght) = (arr[0], arr[1])
+            { Value = None; Operation = "*"; Right = Some(convert(lft)); Left = Some(convert(rght))}
+        elif not (IsOne '/' line) then
+            let arr = line.Split('/');
+            let (lft, rght) = (arr[0], arr[1])
+            { Value = None; Operation = "/"; Right = Some(convert(lft)); Left = Some(convert(rght))}
+        elif not (IsOne '+' line) then
+            let arr = line.Split('+');
+            let (lft, rght) = (arr[0], arr[1])
+            { Value = None; Operation = "+"; Right = Some(convert(lft)); Left = Some(convert(rght))}
+        else
+            let arr = line.Split('-');
+            let (lft, rght) = (arr[0], arr[1])
+            { Value = None; Operation = "-"; Right = Some(convert(lft)); Left = Some(convert(rght))}
 
-let derivative (func: float -> float) (points: float[]) =
-    Array.map (fun item -> 
-         limit (fun x -> ((func (item + x) - func item)/ x)) 0 3 Position.Left
-    ) points
+let rec calculate (node: Node) : int =
+    if node.Value.IsSome then 
+        node.Value.Value
+    else
+        match node.Operation with
+        | "*" -> calculate node.Left.Value * calculate node.Right.Value
+        | "/" -> calculate node.Left.Value / calculate node.Right.Value
+        | "+" -> calculate node.Left.Value + calculate node.Right.Value
+        | "-" -> calculate node.Left.Value - calculate node.Right.Value
+        | _ -> failwith "Not working"
 
-let func (x: float) = tan x
 
-printfn "%A" (derivative (fun x -> Math.Pow(x, x) * tan x) [|1..5|])
+let line = "2+2*2"
+let node = convert line
+let result = calculate node
+
+printfn "%s\nResult: %d" (node.ToString()) result
 
 Console.ReadLine() |> ignore
